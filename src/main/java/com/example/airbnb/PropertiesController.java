d5ad8ba08b78e82c30e6485478595dc06d7a9759
Caroline Moraes <carol.2014pess@gmail.com>
2021-11-14 19:09:51 -0300

    Add @CrossOrigin

--- src/main/java/com/example/airbnb/PropertiesController.java
+++ src/main/java/com/example/airbnb/PropertiesController.java
@@ -3,12 +3,14 @@ package com.example.airbnb;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 //import org.springframework.http.HttpStatus;
 //import org.springframework.http.ResponseEntity;
 //import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class PropertiesController {
@@ -21,7 +23,7 @@ public class PropertiesController {
 		List<Accommodations> accommodationList = repository.findByLocalization(localization);
 		return new ResponseEntity<List<Accommodations>>(accommodationList, HttpStatus.OK);
 	}*/
-	
+	@CrossOrigin
 	@RequestMapping(value = "/all", method = RequestMethod.GET, produces = "application/json")
 	public List<Properties> getAllProperties() {
 			return repository.findAll();
